f329657f5c1f91b89a0a1e7d91071ba9e026dd21
Rodin Grajo <scaredmexicanlaborer@protonmail.com>
2022-05-01 23:54:16 -0500

    added main menu

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import mysql from 'mysql2';
 import { EmployeeTracker } from './lib/EmployeeTracker.js'
 
-const sqlConnect = {
-    host: 'localhost',
-    user: 'root',
-    password: 'realpassword',
-    database: 'employeetracker_db'
-}
-
 const init = () => {
+    const sqlConnect = {
+        host: 'localhost',
+        user: 'root',
+        password: 'realpassword',
+        database: 'employeetracker_db'
+    }
+
     const db = mysql.createConnection(sqlConnect, console.log(`\n...connected to employee tracker database\n`));
-    db.end();
+    const employeeTracker = new EmployeeTracker(db);
+    employeeTracker.init();
 }
 
 init();
