8577e5696a3232074b732bf694b511d48fba9f44
Rodin Grajo <scaredmexicanlaborer@protonmail.com>
2022-05-01 22:23:14 -0500

    finished Query.js, starting EmployeeTracker.js

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 import mysql from 'mysql2';
-import inquirer from 'inquirer';
-import chalk from 'chalk';
-import { Query } from './query.js';
+import { EmployeeTracker } from './lib/EmployeeTracker.js'
 
-const welcomeMessage = `
-
-
-                      /\\
-___                  /  \\                  ___
-/   \\     __        /    \\         __     /
-/     \\   /  \\   _ / ${chalk.red(`<()>`)} \\   _   /  \\   /
-     \\_/    \\_/ \\_/________\\_/ \\_/    \\_/
-_________________/__I___I___\\________________
-                /_I___I___I__\\
-               /I___I___I___I_\\
-              /___I___I___I___I\\
-             /__I___I___I___I___\\
-            /_I___I___I___I___I__\\
-           /I___I___I___I___I___I_\\
-          /___I___I___I___I___I___I\\
-         /__I___I___I___I___I___I___\\
-        /_I___I___I___I___I___I___I__\\ 
-        
-                  WELCOME TO
-        
-               EMPLOYEE TRACKER
-`
 const sqlConnect = {
     host: 'localhost',
     user: 'root',
@@ -34,10 +9,7 @@ const sqlConnect = {
 }
 
 const init = () => {
-    console.log(welcomeMessage);
     const db = mysql.createConnection(sqlConnect, console.log(`\n...connected to employee tracker database\n`));
-    const dbQuery = new Query(db);
-    dbQuery.displayEmployeesByDepartment();
     db.end();
 }
 
