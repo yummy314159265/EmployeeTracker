b3117202ef73cc86de2180ef42404d5a7c9c3d6c
Rodin Grajo <scaredmexicanlaborer@protonmail.com>
2022-05-02 23:59:18 -0500

    completed inquirer menus

--- db/schema.sql
+++ db/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE role (
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
-    ON DELETE SET NULL
+    ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -26,7 +26,7 @@ CREATE TABLE employee (
     manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
-    ON DELETE SET NULL,
+    ON DELETE CASCADE,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,34 @@
 INSERT INTO department (name)
-VALUES  ('Sales'),
+VALUES  ('Executive'),
+        ('Sales'),
         ('R&D'),
         ('Customer Service'),
-        ('Management');
+        ('Management'),
+        ('HR'),
+        ('Marketing');
 
 INSERT INTO role (title, salary, department_id)
-VALUES  ('Sales Lead', 120000.21, 1),
-        ('VP', 1032000.23, 4),
-        ('Engineer', 63000.58, 2),
-        ('Customer Service Rep', 32123.45, 3);
+VALUES  ('VP', 1000000, 1),
+        ('Sales Lead', 100000, 2),
+        ('Engineer', 80000, 3),
+        ('Customer Service Rep', 30000, 4),
+        ('Receptionist', 35000, 4),
+        ('Manager', 60000, 5),
+        ('Hiring Manager', 55000, 6),
+        ('Recruiter', 30000, 6),
+        ('Telemarketer', 40000, 7),
+        ('Graphic Designer', 70000, 7);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ('Charlie', 'Murphy', 2, NULL),
-        ('Elon', 'Musk', 3, 1),
-        ('Johnny', 'Depp', 4, 2),
-        ('Bill', 'Gates', 1, NULL);
+VALUES  ('Daria', 'Sloan', 1, NULL),
+        ('Edgar', 'Peacock', 2, 1),
+        ('Liyana', 'Betts', 3, 2),
+        ('Cherise', 'Collier', 4, 3),
+        ('Florrie', 'Kearns', 5, 3),
+        ('Bailey', 'Fleming', 6, 4),
+        ('Devin', 'Martins', 7, 4),
+        ('Sally', 'Nash', 8, 5),
+        ('Oliwier', 'Berg', 9, 5),
+        ('Rebecca', 'Hopper', 10, 5),
+        ('Yannis', 'Quintero', 4, 6),
+        ('Scarlet', 'Sawyer', 1, 6);
